70f5988e8c20d22a3da5dca86189e5d3df7ea2b4
Christian <nshimyumukizachristian@gmail.com>
2023-04-13 14:41:20 +0200

    Update Base Model Initilization with kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,24 @@ from datetime import datetime
 
 class BaseModel():
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at":
+                    if v is str:
+                        setattr(self, k, datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, k ,v)
+                if k == "updated_at":
+                    if v is str:
+                        setattr(self, k, datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, k, v)
+                if k != "__class__":
+                    setattr(self, k, v)
 
     def __str__(self):
         return("[<{}>] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
