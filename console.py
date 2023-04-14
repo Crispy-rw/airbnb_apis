83f412d135b1b4eada51671efb779f4b7e1d1880
Christian <nshimyumukizachristian@gmail.com>
2023-04-14 21:25:35 +0200

    Working Console!

--- console.py
+++ console.py
@@ -1,5 +1,13 @@
 import cmd
 from models.base_model import BaseModel
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.city import City
+from models.amenity import Amenity
+import re
+import sys
 import inspect
 from models import storage
 
